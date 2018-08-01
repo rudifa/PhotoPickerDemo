0e36ba97454f7c2fbb9f5dcec4b4578cd12654e6
rudifa <rudi.farkas@gmail.com>
2018-08-01 12:01:53 +0200

    add toolbar, main view, outlets and actions

--- PhotoPickerDemo/ViewController.swift
+++ PhotoPickerDemo/ViewController.swift
@@ -10,16 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIView!
+
+    @IBAction func takePhoto(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func pickPhoto(_ sender: UIBarButtonItem) {
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
